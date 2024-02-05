a980bbf22b0652499b169fc6fc7ee08c157d8e6b
atiqisrak <atiq.israk@webable.digital>
2024-02-05 13:33:27 +0600

    icons working fine

--- brewly/assets/components/BottomNav.js
+++ brewly/assets/components/BottomNav.js
@@ -1,12 +1,13 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, SafeAreaView, Text } from 'react-native';
 import WelcomeScreen from './WelcomeScreen';
 import OptionsScreen from './OptionsScreen';
 import OrderList from './OrderList';
 import Profile from './Auth/Profile';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
-import { Icon, MD3Colors } from 'react-native-paper';
+import { HomeIcon, ListBulletIcon, BuildingStorefrontIcon, UserIcon } from "react-native-heroicons/outline";
+import PersonalizeOrder from './PersonalizeOrder';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,60 +21,81 @@ const BottomNav = () => {
         });
     }, [navigation]);
 
-
     const tabs = [
         {
-            title: 'Home',
-            icon: <Icon source="camera" size={24} />,
-        },
-        {
             title: 'Options',
-            icon: <Icon source="camera" size={24} />,
             component: OptionsScreen,
         },
         {
             title: 'Shop',
-            icon: <Icon source="camera" size={24} />,
             component: OrderList,
         },
         {
+            title: 'Home',
+            component: WelcomeScreen,
+        },
+        {
+            title: 'Personalize',
+            component: PersonalizeOrder,
+        },
+        {
             title: 'Profile',
-            icon: <Icon source="camera" size={24} />,
             component: Profile,
         },
     ];
 
+    const renderTabIcon = (tabTitle, focused) => {
+        switch (tabTitle) {
+            case 'Home 1':
+                return <HomeIcon width={24} height={24} color={focused ? "#fff" : "#5c3408"} />;
+            case 'Options':
+                return <ListBulletIcon width={24} height={24} color={focused ? "#fff" : "#5c3408"} />;
+            case 'Shop':
+                return <BuildingStorefrontIcon width={24} height={24} color={focused ? "#fff" : "#5c3408"} />;
+            case 'Home':
+                return <HomeIcon width={24} height={24} color={focused ? "#fff" : "#5c3408"} />;
+            case 'Profile':
+                return <UserIcon width={24} height={24} color={focused ? "#fff" : "#5c3408"} />;
+            default:
+                return null;
+        }
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Tab.Navigator
-                initialRouteName="Home"
-                tabBarOptions={{
-                    showLabel: false,
-                    style: {
-                        position: 'absolute',
-                        bottom: 25,
-                        left: 20,
-                        right: 20,
-                        elevation: 0,
-                        backgroundColor: '#ffffff',
-                        borderRadius: 15,
-                        height: 90,
-                        ...styles.shadow,
+                initialRouteName='Home'
+                screenOptions={({ route }) => ({
+                    tabBarShowLabel: false,
+                    tabBarStyle: {
+                        height: 60,
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        borderRadius: 12,
+                        marginTop: -50,
                     },
-                }}
+                    tabBarIcon: ({ focused }) => {
+                        const tabBackgroundColor = focused ? "#5c3408" : "transparent";
+                        return (
+                            <TouchableOpacity
+                                style={[styles.tabButton,
+                                { backgroundColor: tabBackgroundColor },
+                                { color: focused ? "#fff" : "#5c3408" }
+                                ]}
+                                onPress={() => navigation.navigate(route.name)}
+                            >
+                                {renderTabIcon(route.name, focused)}
+                            </TouchableOpacity>
+                        );
+                    }
+                })}
             >
                 {tabs.map((tab, index) => (
                     <Tab.Screen
                         key={index}
                         name={tab.title}
                         component={tab.component}
-                        options={{
-                            tabBarIcon: ({ focused }) => (
-                                <View style={{ alignItems: 'center', justifyContent: 'center', top: 5 }}>
-                                    {tab.icon}
-                                </View>
-                            ),
-                        }}
                     />
                 ))}
             </Tab.Navigator>
@@ -83,11 +105,12 @@ const BottomNav = () => {
 
 const styles = StyleSheet.create({
     tabButton: {
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 12,
+        paddingHorizontal: 10,
     },
 });
 export default BottomNav;
--- brewly/assets/components/PersonalizeOrder.js
+++ brewly/assets/components/PersonalizeOrder.js
@@ -1,7 +1,7 @@
 // PersonalizeOrder.js
 
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { globalStyles } from '../styles/globalStyles';
 
 const PersonalizeOrder = ({ route, navigation }) => {
@@ -46,49 +46,66 @@ const PersonalizeOrder = ({ route, navigation }) => {
     };
 
     return (
-        <View style={globalStyles.container}>
-            <Text style={globalStyles.heading}>Customize your order:</Text>
+        <SafeAreaView style={styles.scrollViewContainer}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={globalStyles.heading}>Customize your order:</Text>
 
-            {option === 'Water' && renderButtons('temperature', ['Hot', 'Normal', 'Cold'])}
+                    {option === 'Water' && renderButtons('temperature', ['Hot', 'Normal', 'Cold'])}
 
-            {option === 'Tea' &&
-                <>
-                    <Text style={globalStyles.heading}>Sugar (Spoon):</Text>
-                    {renderButtons('spoon', ['None', 'Half', '1', '2'])}
+                    {option === 'Tea' &&
+                        <>
+                            <Text style={globalStyles.heading}>Sugar (Spoon):</Text>
+                            {renderButtons('spoon', ['None', 'Half', '1', '2'])}
 
-                    <Text style={globalStyles.heading}>Amount:</Text>
-                    {renderButtons('amount', ['Half', 'Full'])}
-                </>
-            }
+                            <Text style={globalStyles.heading}>Amount:</Text>
+                            {renderButtons('amount', ['Half', 'Full'])}
+                        </>
+                    }
 
-            {option === 'Coffee' &&
-                <>
-                    <Text style={globalStyles.heading}>Sugar (Spoon):</Text>
-                    {renderButtons('spoon', ['None', 'Half', '1', '2'])}
+                    {option === 'Coffee' &&
+                        <>
+                            <Text style={globalStyles.heading}>Sugar (Spoon):</Text>
+                            {renderButtons('spoon', ['None', 'Half', '1', '2'])}
 
-                    <Text style={globalStyles.heading}>Amount:</Text>
-                    {renderButtons('amount', ['Half', 'Full'])}
+                            <Text style={globalStyles.heading}>Amount:</Text>
+                            {renderButtons('amount', ['Half', 'Full'])}
 
-                    <Text style={globalStyles.heading}>Milk:</Text>
-                    {renderButtons('milk', ['Yes', 'No'])}
-                </>
-            }
+                            <Text style={globalStyles.heading}>Milk:</Text>
+                            {renderButtons('milk', ['Yes', 'No'])}
+                        </>
+                    }
 
-            {option === 'Biscuit' &&
-                <>
-                    <Text style={globalStyles.heading}>Biscuit:</Text>
-                    {renderButtons('biscuit', ['1', '2'])}
-                </>
-            }
+                    {option === 'Biscuit' &&
+                        <>
+                            <Text style={globalStyles.heading}>Biscuit:</Text>
+                            {renderButtons('biscuit', ['1', '2'])}
+                        </>
+                    }
 
-            <TouchableOpacity
-                style={globalStyles.submitButton}
-                onPress={submitCustomization}
-            >
-                <Text style={globalStyles.buttonText}>Submit Customization</Text>
-            </TouchableOpacity>
-        </View>
+                    <TouchableOpacity
+                        style={globalStyles.submitButton}
+                        onPress={submitCustomization}
+                    >
+                        <Text style={globalStyles.buttonText}>Submit Customization</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
+const styles = StyleSheet.create({
+    scrollViewContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+});
 export default PersonalizeOrder;
